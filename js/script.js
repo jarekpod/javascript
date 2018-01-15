e097e1b344496795f365b2013825310a4726b34c
Jaroslaw Podgorski <jarekpod@gmail.com>
2018-01-15 14:50:14 +0100

    update javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name)
+var a = prompt('first digit');
+var h = prompt('secend digit');
+var a ,
+	h ,
+	triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea +'');
+alert (" triangle field is: " + triangleArea );
