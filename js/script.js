1a756041341b9ad744cea1e3b09be1efb0eaab63
Jaroslaw Podgorski <jarekpod@gmail.com>
2018-01-22 22:16:16 +0100

    work 8.6

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
-var a = prompt('first digit');
-var h = prompt('secend digit');
-var a ,
-	h ,
-	triangleArea = a*h/2;
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea +'');
-alert (" triangle field is: " + triangleArea );
+var a = prompt('Enter first number');
+var b = prompt('second number');
+var value = (a * a) - (2 * a * b) + (b * b);
+
+console.log(value);
+
+
+if  (value > 0) {
+    console.log('result is positive'); 
+}   
+else if (value < 0) {
+	console.log('result is negative');
+}
+else {
+	console.log('value is equal to zero')
+}
+
+
